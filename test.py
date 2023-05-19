4034c6c00e661b521749dba5c4b8fc3b101746c1
Ben Hutchins <117102154+benhutchmet@users.noreply.github.com>
2023-05-19 10:17:45 +0000

    updated the test function to verify whether packages are installed

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import xarray as xr
+import cartopy.crs as ccrs
+
 # write a function which prints the sum of two numbers
 def sum(a,b):
+    # write a docstring
+    """ This function returns the sum of two numbers """
+    # print the types of a and b
+    print(type(a))
+    print(type(b))
     return a+b
 
 print(sum(1,2))
